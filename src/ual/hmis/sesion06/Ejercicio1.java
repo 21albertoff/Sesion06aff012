a77bb8f415c86c68cbb36150c9424b70a9fc0765
Alberto Fuentes <aff012@inlumine.ual.es>
2020-05-07 12:50:51 +0200

    Actualizacion ejercici1

--- src/ual/hmis/sesion06/Ejercicio1.java
+++ src/ual/hmis/sesion06/Ejercicio1.java
@@ -11,19 +11,19 @@ public class Ejercicio1 {
 * @param x
 * @return resultado
 */
-public int transformar(final int x) {
-final int tres = 3;
-final int cinco = 5;
-int resultado = 0;
-if (x % 2 == 0) {
-	resultado = transformar(x / 2);
-	} else if (x % tres == 0) {
-		resultado = transformar(x / tres);
-		} else if (x % cinco == 0) {
-			resultado = transformar(x / cinco);
-			} else {
-				return x;
-				}
-return resultado;
-}
+	public int transformar(final int x) {
+		final int tres = 3;
+		final int cinco = 5;
+		int resultado = 0;
+		if (x % 2 == 0) {
+			resultado = transformar(x / 2);
+			} else if (x % tres == 0) {
+				resultado = transformar(x / tres);
+				} else if (x % cinco == 0) {
+					resultado = transformar(x / cinco);
+					} else {
+						return x;
+						}
+		return resultado;
+	}
 }
